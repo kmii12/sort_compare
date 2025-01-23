d3d4a9c2cd4e7c50b0c099bb4d93a4700422f84c
mk <kanzakimiki.12@gmail.com>
2025-01-23 16:42:13 +0900

    土台完成

---
+++ sort_compare/src/app/BubbleSort/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./style.module.css";
+
+export default function BubbleSort() {
+  return (
+    <>
+      <h1>バブルソート</h1>
+    </>
+  );
+}
--- sort_compare/src/app/page.tsx
+++ sort_compare/src/app/page.tsx
@@ -1,16 +1,27 @@
 // import Image from "next/image";
-import Link from "next/Link";
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <p></p>
-      <Link href="../">Bubble Sort</Link>
-      <Link href="../">Selection Sort</Link>
-      <Link href="../">Insertion Sort</Link>
-      <Link href="../">Merge Sort</Link>
-      <Link href="../">組み込みオブジェクトのSort</Link>
+      <div className={styles.flexContainer}>
+        <Link className={styles.linkBtn} href={`/BubbleSort`}>
+          Bubble Sort
+        </Link>
+        <Link className={styles.linkBtn} href={`/SelectionSort`}>
+          Selection Sort
+        </Link>
+        <Link className={styles.linkBtn} href={`/InsertionSort`}>
+          Insertion Sort
+        </Link>
+        <Link className={styles.linkBtn} href={`/MergeSort`}>
+          Merge Sort
+        </Link>
+        <Link className={styles.linkBtn} href={`/ObjectSort`}>
+          組み込みオブジェクトのSort
+        </Link>
+      </div>
     </div>
   );
 }
